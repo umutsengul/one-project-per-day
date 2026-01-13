2277e91fbfdea37e95bb5b27f68c2640b071d40b
asdasda69 <umutsengul.contact@gmail.com>
2026-01-13 10:16:21 +0300

    new project

--- randomNumGenerator/script.js
+++ randomNumGenerator/script.js
@@ -1,7 +1,11 @@
 const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
+const myLabel = document.getElementsByClassName("myLabel");
 
+min = 1;
+max = 6;
 myButton.addEventListener("click", () => {
-  const randomNum = Math.floor(Math.random() * 6) + 1;
-  myLabel.textContent = randomNum;
+  const randomNum = Math.floor(Math.random() * max) + min;
+  const randomNum2 = Math.floor(Math.random() * max) + min;
+  label1.textContent = randomNum;
+  label2.textContent = randomNum2;
 });
